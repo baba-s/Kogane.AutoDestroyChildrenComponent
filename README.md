df6263db23f85e03fc490f897f0ef937dd7b3cae
baba-s <shotababa1206@gmail.com>
2022-09-18 09:17:31 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.AutoDestroyChildrenComponent
+﻿# Kogane Auto Destroy Children Component
+
+シーン開始時に子オブジェクトをすべて削除するコンポーネント
---
+++ Runtime/AutoDestroyChildrenComponent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Kogane.Internal
+{
+    /// <summary>
+    /// シーン開始時に子オブジェクトをすべて削除するコンポーネント
+    /// </summary>
+    [DefaultExecutionOrder( -1000 )]
+    [DisallowMultipleComponent]
+    internal sealed class AutoDestroyChildrenComponent : MonoBehaviour
+    {
+        //================================================================================
+        // 関数
+        //================================================================================
+        /// <summary>
+        /// 初期化される時に呼び出されます
+        /// </summary>
+        private void Awake()
+        {
+            foreach ( Transform child in transform )
+            {
+                Destroy( child.gameObject );
+            }
+        }
+    }
+}
